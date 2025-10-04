074b5cfbc6dfadfb2f69bd3fbac09187386e6b0c
Shefin <shefinptr@gmail.com>
2025-10-04 23:06:02 +0530

    react forms prevent default

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import "./App.css";
 
 const App = () => {
   const [name, setName] = useState("")
+  const [header, setHeader] = useState("")
 
   function HandleChange(event) {
-    setName(event.target.value)
-    
+    setName(event.target.value)    
+  }
+
+  function HandleClick(event){
+    setHeader(name);
+
+    event.preventDefault();
   }
    
   return (
     <div className='container'>
-      <h1>Hello {name}</h1>
-      <input 
-        onChange={HandleChange}
-        type="text" 
-        placeholder="What's your name?" 
-      />
-      <button>Submit</button>
+      <h1>Hello {header}</h1>
+
+      <form onSubmit={HandleClick}>
+        <input
+          onChange={HandleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 };
